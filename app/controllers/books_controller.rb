6789285fd73387b568d02844fcd189d1355da985
EC2 Default User <ec2-user@ip-172-31-8-88.ec2.internal>
2021-11-11 07:24:16 +0000

    [Finish]4-9課題提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,18 @@
 class BooksController < ApplicationController
   def index
     @books=Book.all
-  end
-  
-  def new
     @book=Book.new
   end
   
+  
   def create
-    book=Book.new(book_params)
-    if book.save
+    @book=Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
+      @books=Book.all
       render :index
-      
     end
   end
 
@@ -27,12 +25,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book=Book.find(params[:id])
-    if book.update(book_params)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render :books_path
+      render :edit
     end
   end
   
@@ -46,7 +44,7 @@ class BooksController < ApplicationController
   private
   # ストロングパラメータ
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 
