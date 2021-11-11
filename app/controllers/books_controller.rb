80e938890622dd2ca4e44dc3905ca81ff119f4d3
EC2 Default User <ec2-user@ip-172-31-8-88.ec2.internal>
2021-11-11 02:46:28 +0000

    [Update]4-9課題フラッシュメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,18 @@ class BooksController < ApplicationController
   end
   
   def new
-    book=Book.new
+    @book=Book.new
   end
   
   def create
     book=Book.new(book_params)
-    book.save
-    redirect_to book_path(@book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :index
+      
+    end
   end
 
   def show
@@ -18,7 +23,25 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
+  
+  def update
+    book=Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :books_path
+    end
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
   
   private
   # ストロングパラメータ
